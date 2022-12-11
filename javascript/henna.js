28e8c7e62445fe00824d2f1647f0ae3da941bb0e
its-jd <jaydipdavara1@gmail.com>
2022-12-11 13:07:57 +0100

    added missing javascript files

---
+++ javascript/henna.js
@@ -0,0 +1,23 @@
+function myFunction(th) {
+    var a = document.getElementsByTagName('a')[1];
+    window.open(th.name,'_blank');
+}
+function myself(th1) {
+    var a = document.getElementsByTagName('a')[3];
+    window.open(th1.name1)
+}
+
+function loadContacatUS() {
+    window.open('.\\contactUs.html')
+}
+
+function loadAboutUS() {
+    window.open('.\\aboutUs.html')
+    
+}
+function loadBook() {
+    window.open('.\\calender.html')
+}
+function subclick(){
+    alert('Your verification is done!!!')
+}
